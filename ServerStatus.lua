f99befedc798753f5e4e20a1e54424a76c426bb4
FleMo93 <florianhammer@hotmail.de>
2021-03-10 19:56:56 +0100

    Extend cloud information

--- ServerStatus.lua
+++ ServerStatus.lua
@@ -52,9 +52,9 @@ function ServerStatus.writeStatus()
     serverStatus.weather.season = options.weather.season
 
     serverStatus.weather.clouds = {}
-    serverStatus.weather.clouds = options.weather.clouds.thickness
-    serverStatus.weather.clouds = options.weather.clouds.density
-    serverStatus.weather.clouds = options.weather.clouds.base
+    serverStatus.weather.clouds.thickness = options.weather.clouds.thickness
+    serverStatus.weather.clouds.density = options.weather.clouds.density
+    serverStatus.weather.clouds.base = options.weather.clouds.base
     serverStatus.time = options.start_time + math.floor(DCS.getModelTime())
 
     local filePath = lfs.writedir() .. 'server-status.json'
