3a58240aef139b6e7235f3e5de0c87e1242e37ec
FleMo93 <florianhammer@hotmail.de>
2021-04-05 17:35:58 +0200

    Translate message of the day

--- MessageOfTheDay.lua
+++ MessageOfTheDay.lua
@@ -3,9 +3,9 @@ MessageOfTheDay = {}
 
 function MessageOfTheDay.OnPlayerConnect(id)
     if DCS.isServer() then
-        net.send_chat_to("Willkommen beim Kellergeschwader", id)
-        net.send_chat_to("Besuche uns auf https://kellergeschwader.com")
-        net.send_chat_to("Tippe '--help' für verfügbare chat Befehle", id)
+        net.send_chat_to("Welcome to the Kellergeschwader", id)
+        net.send_chat_to("Visit us on https://kellergeschwader.com")
+        net.send_chat_to("Type '--help' for available chat commands", id)
     end
 end
 
