6f123a23e65b0bc2f3fa84d4e9af6525d3ece138
FleMo93 <florianhammer@hotmail.de>
2021-04-05 17:37:37 +0200

    Fix message of the day

--- MessageOfTheDay.lua
+++ MessageOfTheDay.lua
@@ -4,7 +4,7 @@ MessageOfTheDay = {}
 function MessageOfTheDay.OnPlayerConnect(id)
     if DCS.isServer() then
         net.send_chat_to("Welcome to the Kellergeschwader", id)
-        net.send_chat_to("Visit us on https://kellergeschwader.com")
+        net.send_chat_to("Visit us on https://kellergeschwader.com", id)
         net.send_chat_to("Type '--help' for available chat commands", id)
     end
 end
